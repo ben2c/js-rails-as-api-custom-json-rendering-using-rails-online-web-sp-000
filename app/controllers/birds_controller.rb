2ca807072d019ae0d8b5427dfebba4b4b54be33a
Benjamin Cheng <bencheng200@gmail.com>
2021-05-26 22:56:07 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -10,5 +10,6 @@ class BirdsController < ApplicationController
       render json: {id: bird.id, name: bird.name, species: bird.species }
     else
       render json: {message: "Bird not found" }
+    end
   end
 end
